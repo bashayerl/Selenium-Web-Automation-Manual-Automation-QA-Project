f62663b87cde7cf577781891f0b7df73f229689b
bashayerl <bashayer.lafii@gmail.com>
2025-12-18 21:52:33 +0300

    Selenium CI pipeline

--- src/test/java/TEST/WepFormTest.java
+++ src/test/java/TEST/WepFormTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebElement;
@@ -79,6 +81,19 @@ public class WepFormTest {
         WebElement range = driver.findElement(By.name("my-range"));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].value=60", range);
+
+        driver.findElement(By.xpath("/html/body/main/div/form/div/div[1]/div/a")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/ul/li[72]/a"))).click();
+
+        Actions actions = new Actions(driver);
+        WebElement s = driver.findElement(By.id("leftitem-1"));
+        WebElement S2 = driver.findElement(By.id("rightitem-3"));
+       actions.dragAndDrop(s,S2).perform();
+
+
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
 
 //submit
         driver.findElement(By.xpath("/html/body/main/div/form/div/div[2]/button")).click();
